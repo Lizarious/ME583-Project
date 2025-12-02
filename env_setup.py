5207d5388721dfc238073a988d188fa11bb6a99d
Lizarious <samrkronk@gmail.com>
2025-12-01 17:31:37 -0800

    plot working

---
+++ env_setup.py
@@ -0,0 +1,99 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+# Arbitrary "mountainous terrain" function to promote curvy trajectory
+def terrain_function(x, y, amp=2, freq=.2):
+    return amp * np.sin(freq*x) * np.cos(freq*y) - amp * np.sin(freq/2 * x+2) * np.cos(freq*y+3)
+
+# Potential field functions
+def attractive_force(pos, target):
+    return target - pos
+
+def repulsive_force(pos, obstacles, radius):
+    force = np.zeros(2)
+    for obs in obstacles:
+        direction = pos - obs
+        distance = np.linalg.norm(direction)
+        if distance < radius:
+            repulse = direction / (distance + 1e-6)
+            force += repulse
+    return force
+
+def environment_gen(num_obstacles=10,map_size=10,step_size=0.1,seed=None,return_markers=False,target=False):
+
+    if seed:
+        print("Seed Set")
+        np.random.seed(seed)
+        
+    # Parameters
+    obstacle_radius = 2
+    momentum = 0.90
+    max_iters = map_size*50
+    goal_threshold = 3
+    waypoint_threshold = 3
+
+    # Generate obstacles
+    obstacles = np.random.uniform(map_size*0.05, map_size-map_size*0.05, size=(num_obstacles, 2))
+
+    if target:
+
+        # Start, goal, and random waypoints
+        start = np.array([1.0, 1.0])
+        goal = np.array([map_size, map_size])
+        waypoint1 = np.array([np.random.uniform(map_size/5, map_size/3), np.random.uniform(map_size/5, map_size/1.2)])
+        waypoint2 = np.array([np.random.uniform(map_size/1.5, map_size/1.2), np.random.uniform(map_size/5, map_size/1.2)])
+
+        # Initial position and random direction
+        position = start.copy()
+        velocity = np.abs(np.random.normal(.1, 1.0, size=2))
+        velocity /= np.linalg.norm(velocity) + 1e-6
+
+        # Path list
+        path = [position.copy()]
+        hit_waypoint1, hit_waypoint2 = False, False
+
+        for i in range(max_iters):
+            # Determine current target
+            if not hit_waypoint1:
+                target = waypoint1
+                if np.linalg.norm(position - waypoint1) < waypoint_threshold: hit_waypoint1 = True
+            elif not hit_waypoint2:
+                target = waypoint2
+                if np.linalg.norm(position - waypoint2) < waypoint_threshold: hit_waypoint2 = True
+            else:
+                target = goal
+
+            # Forces
+            to_target = attractive_force(position, target)
+            #away_from_obstacles = repulsive_force(position, obstacles, obstacle_radius)
+
+            # Create gradient from forces and terrain function
+            grad = to_target + terrain_function(position[0], position[1]) * np.array([1, 1]) #+ away_from_obstacles
+
+            # Smooth path w/ momentum
+            grad = grad / (np.linalg.norm(grad) + 1e-6)
+            velocity = momentum * velocity + (1 - momentum) * grad
+            velocity = velocity / (np.linalg.norm(velocity) + 1e-6)
+
+            # Update position
+            position += step_size * velocity
+            path.append(position.copy())
+
+            # Stop if goal reached
+            if hit_waypoint1 and hit_waypoint2 and np.linalg.norm(position - goal) < goal_threshold:
+                print(f"Reached goal in {i+1} steps")
+            """for j in range(max_iters-i):
+                path.append(position.copy())"""
+            break
+            
+    
+    if return_markers:
+        markers = {"start":{"threshold":0,"pos":start},
+                   "waypoint1":{"threshold":waypoint_threshold,"pos":waypoint1},
+                   "waypoint2":{"threshold":waypoint_threshold,"pos":waypoint2},
+                   "goal":{"threshold":goal_threshold,"pos":goal}}
+        return obstacles, np.array(path), markers
+    else:
+        return obstacles
---
+++ map.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def update_plot(i, robot_trajectory, robot_future_traj, robot_controls, robot_radius, obstacles, obstacle_radius, acc_lims, vel_lims, num_time_steps, fig, axs):
+    axs[0].cla()
+    axs[1].cla()
+    
+    robot_position = robot_trajectory[i, :2]
+    #axs[0].add_patch(plt.Circle(robot_position, robot_radius/2, color='C0', alpha=0.4, label='Drone Pos'))
+    #axs[0].add_patch(plt.Circle(robot_position, sensor_range, color='C0', alpha=0.2, label='Obst. Sensor Range'))
+
+    for obst in range(obstacles.shape[0]):
+        axs[0].add_patch(plt.Circle(obstacles[obst], obstacle_radius[obst], color='C1', alpha=0.4))
+    
+    axs[0].plot(obstacles[:, 0], obstacles[:, 1],'g^', label='Obstacles')
+    axs[0].plot(robot_trajectory[:, 0], robot_trajectory[:, 1], color='black')
+    #axs[0].plot(robot_future_traj[i][:, 0], robot_future_traj[i][:, 1], "o-", markersize=3, color='red')
+    #axs[0].plot(sqp_list[i][j][:, 0], sqp_list[i][j][:, 1], "o-", markersize=3, color='green', alpha=0.4)
+    #axs[0].plot(path[:, 0], path[:, 1], 'b--', label='Target Path')
+    axs[0].scatter(robot_trajectory[i:i+1, 0], robot_trajectory[i:i+1, 1], s=30, color='C0')
+    axs[0].grid()
+    axs[0].legend()
+    axs[0].axis("equal")
+    axs[0].set_title("Simulation")
+
+    axs[1].plot(robot_controls)
+    axs[1].scatter([i], robot_controls[i:i+1, 0], label="Acceleration X", color='C0')
+    axs[1].scatter([i], robot_controls[i:i+1, 1], label="Acceleration Y", color='C1')
+    axs[1].hlines([acc_lims[0], acc_lims[1]], 0, num_time_steps-1, color='C1', linestyle='--')
+    #axs[1].plot(robot_trajectory[:, -1], markersize=3, color='C2', label="Velocity")
+    #axs[1].scatter([i], robot_trajectory[i:i+1, 3], color='C2')
+    axs[1].hlines([vel_lims[0], vel_lims[1]], 0, num_time_steps-1, color='C2', linestyle='--')
+    axs[1].set_xlim([0, num_time_steps])
+    axs[1].set_ylim([-5, 5])
+    axs[1].set_title("States and Inputs")
+    axs[1].legend()
+    axs[1].grid()
+
+    fig.canvas.draw_idle()
